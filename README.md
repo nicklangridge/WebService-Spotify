5168bddabd8e2e82527403e68a2bcf756886a657
Nick Langridge <nicklangridge@hotmail.com>
2014-06-24 13:42:15 +0100

    more dev

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 WebService-Spotify
 ==================
+
+Work in progress...
+
+This will be a Perl port of the Python Spotify web API https://github.com/plamere/spotipy
--- lib/WebService/Spotify.pm
+++ lib/WebService/Spotify.pm
@@ -26,12 +26,22 @@ has 'user_agent' => (
   default => method { LWP::UserAgent->new->(agent => 'WebService::Spotify/' . $VERSION) }
 );
 
-method get ($method, $args) {
-  my $uri = URI->new( $self->prefix . $method );
-  $uri->query_param( $_, $args{$_} ) for keys %$args;
+method get ($method, %args) {
+  my $url = $method =~ /^http/ ? $method : $self->prefix . $method;
+
+  my $uri = URI->new( $url );
+  $uri->query_param( $_, $args{$_} ) for keys %args;
 
   my $response = $self->user_agent->get( $uri->as_string );
   my $data     = from_json( $response->content );
+
+  return $data;
+}
+
+method post ($method, :$payload, %args) {
+  my $url = $method =~ /^http/ ? $method : $self->prefix . $method;
+
+  ## do the post...
 
   return $data;
 }
@@ -46,7 +56,7 @@ method previous ($result) {
 
 method track ($track) {
   my $track_id = $self._get_id('track', $track);
-  return $self.get('tracks/' . $track_id);
+  return $self.get("tracks/$track_id");
 }
 
 method tracks (@tracks) {  
@@ -56,7 +66,7 @@ method tracks (@tracks) {
 
 method artist ($artist) {
   my $artist_id = $self._get_id('artist', $artist);
-  return $self.get('artists/' . $artist_id);
+  return $self.get("artists/$artist_id");
 }
 
 method artists ($artists) {  
@@ -66,7 +76,58 @@ method artists ($artists) {
 
 method artist_albums ($artist, :$album_type, :$country, :$limit = 20, :$offset = 0) {
   my $artist_id = $self._get_id('artist', $artist);
-  return self.get('artists/' . $artist_id . '/albums', { album_type => $album_type, country => $country, limit => $limit, offset => $offset })
+  return self.get("artists/$artist_id/albums", album_type => $album_type, country => $country, limit => $limit, offset => $offset);
+}
+
+method artist_top_tracks ($artist, :$country = 'US') {
+  my $artist_id = $self._get_id('artist', $artist);
+  return self.get("artists/$artist_id/top-tracks", country => $country);
+}
+
+method album ($album) {
+  my $album_id = $self._get_id('album', $album);
+  return $self.get("albums/$album_id");
+}
+
+method album_tracks ($album) {
+  my $album_id = $self._get_id('album', $album);
+  return $self.get("albums/$album_id/tracks");
+}
+
+method albums ($albums) {
+  my @album_ids = map { $self._get_id('album', $_) } @albums;
+  return $self.get('albums/?ids=' . join(',', @album_ids));
+}
+
+method search ($q, :$limit = 10, :$offset = 0, :$type = 'track') {
+  return self.get('search', q => $q, limit => $limit, offset => $offset, type => $type);
+}
+
+method user ($q, $user_id) {
+  return self.get("users/$user_id");
+}
+
+method user_playlists($user_id) {
+  return $self.get("users/$user_id/playlists");
+}
+
+method user_playlist($user_id, :$playlist_id, :$fields) {
+  my $method = $playlist_id ? "playlists/$playlist_id" : "starred";
+  return $self.get("users/$user_id/$method", $fields => $fields);
+}
+
+method user_playlist_create($user_id, $name, :$public = 1) {
+  my $data = { 'name' => $name, 'public' => $public };
+  return $self.post("users/$user_id/playlists", payload => $data);
+}
+
+method user_playlist_add_tracks($user_id, $playlist_id, $tracks, :$position) {
+  my $data = { 'name' => $name, 'public' => $public };
+  return $self.post("users/$user_id/playlists/$playlist_id/tracks", payload => $tracks, $position => $position);
+}
+
+method me ($user_id, $playlist_id, $tracks, :$position) {
+  return $self.get('me/');
 }
 
 method _get_id {
