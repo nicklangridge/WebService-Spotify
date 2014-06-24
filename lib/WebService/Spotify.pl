7d6a30779e9701590063335a2986ec0c6588b10c
Nick Langridge <nicklangridge@hotmail.com>
2014-06-24 11:23:41 +0100

    more dev

--- lib/WebService/Spotify.pl
+++ lib/WebService/Spotify.pl
@@ -1,42 +1,90 @@
 package WebService::Spotify;
-use strict;
-use warnings;
-use URI;
-use JSON;
+use Moose;
+use MooseX::StrictConstructor;
+use Method::Signatures;
 use LWP::UserAgent;
+use URI::QueryParam;
+use JSON;
 
 our $VERSION = '0.001';
 
-sub new {
-  my ($class, $args) = @_;
-  
-  return bless {
-    api_root   => $args{api_root} || 'https://api.spotify.com/v1/',
-    auth_token => $args{auth_token},
-    user_agent => $args{user_agent}
-  }, $class;
-}
+has 'prefix' => (
+  is => 'rw',
+  isa => 'Str',
+  default => 'https://api.spotify.com/v1/'
+);
 
-sub api_root   { return $_[0]->{api_root}   };
-sub auth_token { return $_[0]->{auth_token} };
+has 'auth' => (
+  is => 'rw',
+  isa => 'Str',
+);
 
-sub user_agent { 
-  my $self = shift;
-  
-  $self->{user_agent} ||= LWP::UserAgent->new->(agent => 'WebService::Spotify/' . $VERSION);
-  return $self->{user_agent};
-};
+has 'user_agent' => (
+  is => 'rw',
+  isa => 'LWP::UserAgent',
+  required => 0,
+  default => method { LWP::UserAgent->new->(agent => 'WebService::Spotify/' . $VERSION) }
+);
 
-sub get {
-  my ($self, $method, $args) = @_;
-  
-  my $uri = URI->new( $self->api_root . $method );
+method get ($method, $args) {
+  my $uri = URI->new( $self->prefix . $method );
   $uri->query_param( $_, $args{$_} ) for keys %$args;
 
   my $response = $self->user_agent->get( $uri->as_string );
   my $data     = from_json( $response->content );
 
   return $data;
+}
+
+method next ($result) {
+   return $self.get($result{next}) if $result{next};
+}
+
+method previous ($result) {
+   return $self.get($result{previous}) if $result{previous};
+}
+
+method track ($track) {
+  my $track_id = $self._get_id('track', $track);
+  return $self.get('tracks/' . $track_id);
+}
+
+method tracks (@tracks) {  
+  my @track_ids = map { $self._get_id('track', $_) } @tracks;
+  return $self.get('tracks/?ids=' . join(',', @track_ids));
+}
+
+method artist ($artist) {
+  my $artist_id = $self._get_id('artist', $artist);
+  return $self.get('artists/' . $artist_id);
+}
+
+method artists ($artists) {  
+  my @artist_ids = map { $self._get_id('artist', $_) } @artists;
+  return $self.get('artists/?ids=' . join(',', @artist_ids));
+}
+
+method artist_albums ($artist, :$album_type, :$country, :$limit = 20, :$offset = 0) {
+  my $artist_id = $self._get_id('artist', $artist);
+  return self.get('artists/' . $artist_id . '/albums', { album_type => $album_type, country => $country, limit => $limit, offset => $offset })
+}
+
+method _get_id {
+  my ($self, $type, $id) = @_;
+
+  my @fields = split /:/, $id;
+  if (@fields == 3) {
+    warn "expected id of type $type but found type $fields[2] id" if $type ne $fields[1];
+    return $fields[2];
+  }
+
+  @fields = split /\//, $id;
+  if (@fields >= 3) {
+    warn "expected id of type $type but found type $fields[-2] id" if $type ne $fields[-2];
+    return $fields[-1];
+  }
+
+  return $id;
 }
 
 1;
