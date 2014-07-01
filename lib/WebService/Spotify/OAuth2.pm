8e0c4b0d564c38caa9053d91b6161000f7f70c69
Nick Langridge <nicklangridge@hotmail.com>
2014-07-01 09:17:00 +0100

    Prep for tidy-up relesae

--- lib/WebService/Spotify/OAuth2.pm
+++ lib/WebService/Spotify/OAuth2.pm
@@ -8,7 +8,7 @@ use JSON;
 use MIME::Base64;
 use Data::Dumper;
 
-our $VERSION = '1.000';
+our $VERSION = '1.001';
 
 has 'client_id'     => ( is => 'rw', required => 1 );
 has 'client_secret' => ( is => 'rw', required => 1 );
